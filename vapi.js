541ce83d8d32eee8d7e7b735a3bda365531aaf5f
Suremeo <dkhorton2002@gmail.com>
2019-10-23 17:22:06 -0500

    Handling useragent things

--- vapi.js
+++ vapi.js
@@ -2,6 +2,19 @@ module.exports = (useragent) => {
     let endpoint = "https://api.vastlands.net"
     if (useragent && useragent != "") {
         useragent = `VAPI_NODE-${useragent}`
-        return require("./src/module")(endpoint, useragent)
+        if (useragent.length > 256) {
+            return {
+                Valid: false,
+                Message: "Useragent length > 256"
+            }
+        } else {
+            return require("./src/module")(endpoint, useragent)
+        }
+
+    } else {
+        return {
+            Valid: false,
+            Message: "No useragent supplied"
+        }
     }
 }
